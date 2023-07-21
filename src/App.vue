ad01103de586778a4d24713df377859f65716618
MayaLisnawati <maya_013@smkassalaambandung.sch.id>
2023-07-21 16:40:51 +0700

    latihan vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,142 @@
-<script setup>
-import { ref } from 'vue'
+<template>
+  <div>
+    <h1>Formulir Rapot</h1>
+    <hr>
+  </div>
 
-const msg = "Hello World"
-const rawHtml ="<li>Teks Ini Berwarna Merah</li>"
-let a=10
-let b=5
-let c=a+b
-const link = "https://youtu.be/YT08A8Jucj0"
+  <div>
+    <form @submit.prevent="submitForm">
+      <label for="">Nama</label>
+      <input class="{'is-invalid':isInvalidName}" type="text"
+      name="nama" v-model="siswa.name">
+      <div v-if="isInvalidName" class="eror-message">
+      </div>
+      <br>
+      <label for="">Kelas</label>
+      <input class="{'is-invalid':isInvalidKelas}"
+      type="kelas" name="kelas" v-model="siswa.kelas">
+      <div v-if="isInvalidkelas" class="eror-message">
+      </div>
+      <br>
+      <label for="">Alamat</label>
+      <input class="{'is-invalid':isInvalidAlamat}"
+      type="alamat" name="alamat" v-model="siswa.alamat">
+      <div v-if="isInvalidAlamat" class="eror-message">
+      </div>
+      <br>
+      <label for="">Nilai Matematika</label>
+      <input class="{'is-invalid':isInvalidMatematika}"
+      type="matematika" name="matematika" v-model="siswa.matematika">
+      <div v-if="isInvalidMatematika" class="eror-message">
+      </div>
+      <br>
+      <label for="">Nilai Bahasa Indonesia</label>
+      <input class="{'is-invalid':isInvalidIndonesia}"
+      type="indonesia" name="indonesia" v-model="siswa.indonesia">
+      <div v-if="isInvalidIndonesia" class="eror-message">
+      </div>
+      <br>
+      <label for="">Nilai Ipa</label>
+      <input class="{'is-invalid':isInvalidIpa}"
+      type="ipa" name="ipa" v-model="siswa.ipa">
+      <div v-if="isInvalidIpa" class="eror-message">
+      </div>
+      <br>
+      <button type="submit">Simpan</button>
+    </form>
+    <div v-if="showResult">
+      nama : {{ name }} <br>
+      kelas : {{ kelas }} <br>
+      alamat : {{ alamat }} <br>
+      nilai matematika : {{ matematika }} <br>
+      nilai indonesia : {{ indonesia }} <br>
+      nilai ipa : {{ ipa }} <br>
+      total : {{ total }} <br>
+      keterangan : {{ keterangan }}
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import { ref } from 'vue';
+
+export default {
+  data() {
+    return {
+      siswa: {
+        name: '',
+        kelas: '',
+        alamat: '',
+        matematika: '',
+        indonesia: '',
+        ipa: '',
+        total: '',
+        keterangan: '',
+      },
+      showResult: ref(false),
+      name: ref(''),
+      kelas: ref(''),
+      alamat: ref(''),
+      matematika: ref(''),
+      indonesia: ref(''),
+      ipa: ref(''),
+      total: 0,
+      keterangan: ref(''),
+    };
+  },
+  computed: {
+    isInvalidName() {
+      return this.name.length <=1;
+    },
+    isInvalidEmail() {
+      return this.kelas.length <=1;
+    },
+    isInvalidAlamat() {
+      return this.alamat.length <=1;
+    },
+    isInvalidMatematika() {
+      return this.matematika.length <=1;
+    },
+    isInvalidIndonesia() {
+      return this.indonesia.length <=1;
+    },
+    isInvalidIpa() {
+      return this.ipa.length <=1 ;
+    }
+  },
+  methods: {
+    submitForm() {
+      this.name = this.siswa.name;
+      this.kelas = this.siswa.kelas;
+      this.alamat = this.siswa.alamat;
+      this.matematika = this.siswa.matematika;
+      this.indonesia = this.siswa.indonesia;
+      this.ipa = this.siswa.ipa;
+      if (this.total > 240) {
+        this.keterangan = "Lulus"
+      } else {
+        this.keterangan = "Tidak Lulus"
+      }
+      this.total = parseInt(this.matematika) + parseInt(this.indonesia) + parseInt(this.ipa);
+      this.showResult = true;
+    },
+  },
+};
 </script>
 
-<template>
-  <div v-bind="dynamicId" id="app">
-    <h1>{{ msg }}</h1>
+<style>
+h1 {
+  color: lightskyblue;
+}
 
-    <span style="color: red" v-html="rawHtml" ></span>
-    <button :disabled="isButtonDisabled"><a :href="link"> Youtube </a></button> <br>
-    Bilangan 1 = {{ a }}<br>
-    Bilangan 2 = {{ b }}<br>
-    Hasil = {{ c }}
-  </div>
-</template>
+.eror-message{
+  color:red;
+  margin-top:5px;
+}
+
+.is-invalid {
+  border-color:red;
+}
+</style>
